76bf15fb29f9da161312f64e45726c4e9c009e06
Guillermo <wasanga7@gmail.com>
2017-12-03 13:09:32 +0100

    Update CurvaCorregida.java

--- Principal/CurvaCorregida.java
+++ Principal/CurvaCorregida.java
@@ -1,25 +1,29 @@
 package principal;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class CurvaCorregida implements curva {
-    List<parIV> pts;
 
-    public List<parIV> getPts() {
+	ArrayList<parIV> pts;
+
+	public CurvaCorregida(ArrayList<parIV> pares){
+		pts=pares;
+	}
+
+	public ArrayList<parIV> getPts() {
 		return pts;
 	}
 
-	public void setPts(List<parIV> pts) {
+	public void setPts(ArrayList<parIV> pts) {
 		this.pts = pts;
 	}
 
 	public void mostrarDatos(){
-        int i = 0;
-        for (parIV p : pts){
-            System.out.println("PARIV" + i + "[Intensidad = " + p.getIntensidad() + "] ; [Voltaje = " + p.getVoltaje());
-            i++;
-        }
-    }
-    
-}
+		int i = 0;
+		for (parIV p : pts){
+			System.out.println("PARIV" + i + "[Intensidad = " + p.getIntensidad() + "] ; [Voltaje = " + p.getVoltaje());
+			i++;
+		}
+	}
 
+}
