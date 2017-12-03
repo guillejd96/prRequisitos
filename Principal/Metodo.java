8cf4478c0f2c755cdc6726044b9f4d6a68feecde
Guillermo <wasanga7@gmail.com>
2017-12-03 13:06:56 +0100

    Create Metodo.java

---
+++ Principal/Metodo.java
@@ -0,0 +1,26 @@
+package principal;
+
+import java.util.ArrayList;
+
+public class Metodo {
+
+	public Metodo(){
+
+	}
+
+	public static CurvaCorregida Metodo1(double alfa,double beta,double res,double kappa,double isc,double g1,double g2,double t1,double t2,ArrayList<parIV> pares){
+		ArrayList<parIV> newPares = new ArrayList<>();
+
+		for(parIV p : pares){
+			double i2 = p.getIntensidad() + isc * ( (g2/g1) - 1 ) + alfa * (t2-t1); // Formula para I2[i]
+			double v2 = p.getVoltaje() - res * (i2-p.getIntensidad()) - kappa * i2 * (t2-t1) + beta * (t2-t1); // Formula para V2[i]
+			parIV newP = new parIV(i2, v2);
+			newPares.add(newP);
+		}
+
+		CurvaCorregida cc = new CurvaCorregida(newPares);
+
+		return cc;
+	}
+
+}
