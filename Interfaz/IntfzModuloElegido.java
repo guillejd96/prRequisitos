7d1ad6c969aa3444a8511b797136226776ee3a19
MiguelPelMedina <MiguelPelMedina@users.noreply.github.com>
2017-12-28 14:17:54 +0100

    Update IntfzModuloElegido.java

--- Interfaz/IntfzModuloElegido.java
+++ Interfaz/IntfzModuloElegido.java
@@ -10,6 +10,7 @@ import javax.swing.JPanel;
 
 import principal.*;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JTable;
@@ -21,7 +22,23 @@ import java.awt.Font;
 import java.awt.Choice;
 import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.text.TabableView;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JLayeredPane;
+import javax.swing.border.EtchedBorder;
+import java.awt.ScrollPane;
 
 public class IntfzModuloElegido {
 
@@ -29,7 +46,7 @@ public class IntfzModuloElegido {
 	//EL MODULO OBTENIDO
 	private Modulo mod = new Modulo();
 	private JTable tablaCurvas;
-	private ArrayList<CurvaOriginal> listaDeCurvas; 		//curvas seleccionadas en la campaña
+	private ArrayList<CurvaOriginal> listaDeCurvas; 		//curvas seleccionadas en la campaÃ±a
 	private ArrayList<CurvaOriginal> listaDeCurvasACorregir;//curvas seleccionadas para corregir
 
 	JPanel panelCurva;
@@ -43,6 +60,12 @@ public class IntfzModuloElegido {
 	private JTextField txtfT2;
 	private JTextField txtfIrr1;
 	private JTextField txtfIrr2;
+	
+	private JLayeredPane layeredPane;
+	
+	ChartPanel panelGrafica;
+	private JTable tablaGraf;
+	private JScrollPane scrollPane_1;
 
 	/**
 	 * Launch the application.
@@ -77,7 +100,7 @@ public class IntfzModuloElegido {
 	 */
 	private void initialize(String nombreModulo) throws ClassNotFoundException {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 625, 373);
+		frame.setBounds(100, 100, 1111, 628);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);//evita cerrar todo el proyecto
 		frame.getContentPane().setLayout(null);
 		
@@ -109,97 +132,123 @@ public class IntfzModuloElegido {
 			data[i][6] = co.getVPmax();			//VPmax
 			data[i][7] = co.getFF();			//FF
 			data[i][8] = co.getIdCurva();		//id en la base de datos, necesario para obtener las seleccionadas
-
+			i++;
 		}
 		
 		
 //------PANEL CURVAS	
 		panelCurva = new JPanel();
-		panelCurva.setBounds(0, 0, 609, 334);
+		panelCurva.setBounds(0, 0, 609, 579);
 		frame.getContentPane().add(panelCurva);
 		panelCurva.setLayout(null);
 		DefaultTableModel model = new DefaultTableModel(data, columnName);
-		tablaCurvas = new JTable(model){
+		tablaCurvas = new JTable(model);
+		tablaCurvas.setSelectionMode( ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);	//permite que se seleccionen varias columnas
+		tablaCurvas.addMouseListener(new MouseAdapter() {
+			//ESTE METODO ACTUALIZA LA VENTANA GRAFICA
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				
+				//seleccionamos el objeto
+				int seleccion = tablaCurvas.rowAtPoint(arg0.getPoint());
+				CurvaOriginal a = null;
+				//obtenemos la curva
+				try {
+					a = new CurvaOriginal( (int) tablaCurvas.getValueAt(seleccion, 8));	//coge la id (CAMBIAR ESTA COSA CUANDO SE ACTUALIZE LA BD)
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					JOptionPane.showMessageDialog(null, e.getMessage(),"ERROR!",JOptionPane.ERROR_MESSAGE);
+				}
+				XYSeries seriesA = new XYSeries("par i-v");
+				//datos para la tabla
+				
+				//cogemos el modelo y borramos sus datos anteriores
+				DefaultTableModel temp = (DefaultTableModel) tablaGraf.getModel();	
+				int total = temp.getRowCount()+1;
+				for(int w =0; w<total-1;w++){
+					temp.removeRow(0);
+				}
+		        // Introduccion de datos
+				int i = 0;
+				for( parIV pts : a.getPts()){
+					//parte de la tabla
+					double pot = pts.getVoltaje()*pts.getIntensidad();	//potencia
+					temp.addRow(new Object[]{i+1,
+											Math.floor(pts.getVoltaje() * 100000) / 100000
+											,Math.floor(pts.getIntensidad() * 100000) / 100000
+											,pot});
+					
+					i++;
+					
+					//parte grafica
+					seriesA.add(pts.getIntensidad(), pts.getVoltaje());
+				}
+				
 
-            private static final long serialVersionUID = 1L;
-            @Override
-            public Class getColumnClass(int column) {
-                switch (column) {
-                    case 0:
-                        return int.class;
-                    case 1:
-                        return Boolean.class;
-                    case 2:
-                        return double.class;
-                    case 3:
-                        return double.class;
-                    case 4:
-                        return double.class;
-                    case 5:
-                        return double.class;
-                    case 6:
-                        return double.class;
-                    case 7:
-                        return double.class;
-                    case 8:
-                        return int.class;
-                    default:
-                    	return null;
-                }
-            }
-        };
+		        XYSeriesCollection datasetA = new XYSeriesCollection();
+		        datasetA.addSeries(seriesA);
+
+		        JFreeChart chartA = ChartFactory.createXYLineChart(
+		                "curvaFoltovoltaica", // Título
+		                "Corriente (A)", // Etiqueta Coordenada X
+		                "Tension (V)", // Etiqueta Coordenada Y
+		                datasetA, // Datos
+		                PlotOrientation.VERTICAL,
+		                false, // Muestra la leyenda de los productos (Producto A)
+		                false,
+		                false
+		        );
+				panelGrafica.setChart(chartA);
+				
+			}//end void del click del raton
+		});
 		tablaCurvas.setBounds(20, 11, 414, 183);
 		
 		
 		JScrollPane scrollPane = new JScrollPane(tablaCurvas);
-		scrollPane.setBounds(20, 11, 579, 278);
+		scrollPane.setBounds(20, 22, 579, 512);
 		panelCurva.add(scrollPane);
 		
 				//-----BOTON CORREGIR
 				JButton btnCorregir = new JButton("Corregir");
 				btnCorregir.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
-						//obtener todas las curvas que se han seleccionado
-						for(int i = 0; i < listaDeCurvas.size();i++) {
-							//miro si ha sido seleccionada
-							boolean selected = (Boolean) tablaCurvas.getValueAt(i, 1);
-							
-							if(selected) {
-
-								int id = (int) data[i][8];				//PODRIA DAR FALLO, REVISAR
-								
+						//obtener todas los indices que se han seleccionado
+						int[] curvasSelecionadas = tablaCurvas.getSelectedRows();
+						
+						
+						//Si no se selecciona ninguno
+						if(curvasSelecionadas.length == 0) {
+							JOptionPane.showMessageDialog(null, "No se han seleccionado curvas", "Aviso",JOptionPane.WARNING_MESSAGE);
+						}else {
+							for(int w=0;w<curvasSelecionadas.length;w++){
 								try {
-									listaDeCurvasACorregir.add( new CurvaOriginal(id) );
+									//Basicamente metemos en la lista de curvas corregidas las seleccionasa
+									listaDeCurvasACorregir.add(new CurvaOriginal( (int) tablaCurvas.getValueAt(curvasSelecionadas[w], 8)) );
+									//CAMBIARF ESTA COSA CUANDO CAMBIE LA BD
 								} catch (ClassNotFoundException e1) {
 									// TODO Auto-generated catch block
 									JOptionPane.showMessageDialog(null, e1.getMessage(),"ERROR!",JOptionPane.ERROR_MESSAGE);
 								}
-								
 							}
-						}
-						
-						
-						//Si no se selecciona ninguna
-						if(listaDeCurvasACorregir.isEmpty()) {
-							JOptionPane.showMessageDialog(null, "No se han seleccionado curvas", "Aviso",JOptionPane.WARNING_MESSAGE);
-						}else {
-							//cojo la primera curva y meto sus valores en la segunda pantalla
+							
 							Iterator<CurvaOriginal> ic = listaDeCurvasACorregir.iterator();
 							CurvaOriginal temporal = ic.next();
 							txtfT1.setText(String.valueOf(temporal.getTemp()));
 							txtfIrr1.setText(String.valueOf(temporal.getIrr()));
 							panelCurva.setVisible(false);
 							panelCorreccion.setVisible(true);
+							
 						}
 					}
 				});
-				btnCorregir.setBounds(510, 300, 89, 23);
+				btnCorregir.setBounds(510, 545, 89, 23);
 				panelCurva.add(btnCorregir);					
 									
 
 		//------PANEL CORRECCION
 		panelCorreccion = new JPanel();
-		panelCorreccion.setBounds(0, 0, 609, 334);
+		panelCorreccion.setBounds(0, 0, 609, 579);
 		frame.getContentPane().add(panelCorreccion);
 		panelCorreccion.setLayout(null);
 
@@ -215,7 +264,7 @@ public class IntfzModuloElegido {
 		panelCorreccion.add(lblMtodosDeCorrecin);
 		//-----seleccionador
 		Choice choiceMetodo = new Choice();
-		choiceMetodo.add("IEC60891 método 1");
+		choiceMetodo.add("IEC60891 mÃ©todo 1");
 		choiceMetodo.setBounds(10, 96, 250, 20);
 		panelCorreccion.add(choiceMetodo);
 
@@ -241,13 +290,13 @@ public class IntfzModuloElegido {
 
 		txtfAlpha = new JTextField();
 		txtfAlpha.setBounds(103, 143, 109, 20);
-		txtfAlpha.setText(String.valueOf(mod.getAlfa()));	//valor por defecto del módulo
+		txtfAlpha.setText(String.valueOf(mod.getAlfa()));	//valor por defecto del mÃ³dulo
 		panelCorreccion.add(txtfAlpha);
 		txtfAlpha.setColumns(10);
 
 		txtfBetta = new JTextField();
 		txtfBetta.setBounds(103, 168, 109, 20);
-		txtfBetta.setText(String.valueOf(mod.getBeta()));	//valor por defecto del módulo
+		txtfBetta.setText(String.valueOf(mod.getBeta()));	//valor por defecto del mÃ³dulo
 		panelCorreccion.add(txtfBetta);
 		txtfBetta.setColumns(10);
 
@@ -259,7 +308,7 @@ public class IntfzModuloElegido {
 
 		txtfKappa = new JTextField();
 		txtfKappa.setBounds(103, 218, 109, 20);
-		txtfKappa.setText(String.valueOf(mod.getKappa()));	//valor por defecto del módulo
+		txtfKappa.setText(String.valueOf(mod.getKappa()));	//valor por defecto del mÃ³dulo
 		panelCorreccion.add(txtfKappa);
 		txtfKappa.setColumns(10);
 
@@ -288,7 +337,7 @@ public class IntfzModuloElegido {
 			public void actionPerformed(ActionEvent e) {
 				try {
 
-					if(choiceMetodo.getSelectedItem().equals("IEC60891 método 1")) {
+					if(choiceMetodo.getSelectedItem().equals("IEC60891 mÃ©todo 1")) {
 						//obtenemos la priemra curva
 						Iterator <CurvaOriginal> it = listaDeCurvasACorregir.iterator();
 						CurvaOriginal is = it.next();
@@ -305,7 +354,6 @@ public class IntfzModuloElegido {
 										Double.parseDouble(txtfT2.getText()),
 										is.getPts());
 						
-						Iterator<parIV> ish = curva_corregida.getPts().iterator();
 						
 						IntfzCurvaCorregida cci = new IntfzCurvaCorregida(curva_corregida);
 						cci.newScreen(curva_corregida);
@@ -322,6 +370,8 @@ public class IntfzModuloElegido {
 		JButton btnAtras_correccion = new JButton("Atr\u00E1s");
 		btnAtras_correccion.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				//tambien tiene que vaciar las curvas seleccionadas
+				listaDeCurvasACorregir.clear();
 
 				panelCorreccion.setVisible(false);
 				panelCurva.setVisible(true);
@@ -393,6 +443,58 @@ public class IntfzModuloElegido {
 		lblMedidaIrr2.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblMedidaIrr2.setBounds(457, 221, 46, 14);
 		panelCorreccion.add(lblMedidaIrr2);
+//---------PANEL GRAFICO
+		
+		layeredPane = new JLayeredPane();
+		layeredPane.setBorder(BorderFactory.createTitledBorder(
+                "Representación gráfica"));
+		
+		layeredPane.setBounds(615, 11, 425, 568);
+		frame.getContentPane().add(layeredPane);
+		
+		
+		XYSeries series = new XYSeries("par i-v");
+
+        // Introduccion de datos
+        series.add(1, 1);
+        series.add(2, 6);
+        series.add(3, 3);
+        series.add(4, 10);
+
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(series);
+
+        JFreeChart chart = ChartFactory.createXYLineChart(
+                "curvaFoltovoltaica", // Título
+                "Tension (V)", // Etiqueta Coordenada X
+                "Corriente (A)", // Etiqueta Coordenada Y
+                dataset, // Datos
+                PlotOrientation.VERTICAL,
+                false, // Muestra la leyenda de los productos (Producto A)
+                false,
+                false
+        );
+		
+		
+		
+        panelGrafica = new ChartPanel(chart);
+        panelGrafica.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+        panelGrafica.setBounds(10, 21, 405, 251);
+        layeredPane.add(panelGrafica);
+        panelGrafica.setName("A");
+        
+        String[] columaGraf = {"N","TensiÃ³n(V)","Corriente(A)","Potencia(W)"};
+		
+		
+		DefaultTableModel modelo = new DefaultTableModel(columaGraf, 0);
+        tablaGraf = new JTable(modelo);
+        tablaGraf.setBounds(71, 235, 274, 205);
+        layeredPane.add(tablaGraf);       
+        
+        scrollPane_1 = new JScrollPane(tablaGraf);
+        scrollPane_1.setBounds(10, 283, 405, 274);
+        layeredPane.add(scrollPane_1);
+        
 		
 		
 //-----necesario para la inicializacion
