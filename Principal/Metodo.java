a00d3f084c6ea22e67ad138993326bb89b0bb9e8
Guillermo <wasanga7@gmail.com>
2017-12-29 12:57:54 +0100

    Update Metodo.java

--- Principal/Metodo.java
+++ Principal/Metodo.java
@@ -10,16 +10,15 @@ public class Metodo {
 	}
 
 	public static CurvaCorregida Metodo1(double alfa,double beta,double res,double kappa,double isc,double g1,double g2,double t1,double t2,List<parIV> pares){
-		ArrayList<parIV> newPares = new ArrayList<>();
-
+		String in="",vo="";
 		for(parIV p : pares){
 			double i2 = p.getIntensidad() + isc * ( (g2/g1) - 1 ) + alfa * (t2-t1); // Formula para I2[i]
 			double v2 = p.getVoltaje() - res * (i2-p.getIntensidad()) - kappa * i2 * (t2-t1) + beta * (t2-t1); // Formula para V2[i]
-			parIV newP = new parIV(i2, v2);
-			newPares.add(newP);
+			in+=i2+";";
+			vo+=v2+";";
 		}
 
-		CurvaCorregida cc = new CurvaCorregida(newPares);
+		CurvaCorregida cc = new CurvaCorregida(in,vo); // AQUI HAY QUE TOCAR COSAS
 
 		return cc;
 	}
